688ab0a77abb447b52de4ccdb89f5c21fcf21894
BrenoKuro <brenosla4@gmail.com>
2025-05-19 15:49:16 -0300

    First commit

---
+++ tarefa1.py
@@ -0,0 +1,10 @@
+#Exercícios 
+
+#1-Crie um algoritmo em portugol ou qualquer outra linguagem que receba dois números e mostre a soma deles.
+number1 = int(input("Digite o primeiro número: "))
+number2 = int(input("Digite o segundo número: "))
+
+soma = number1+number2 
+
+print(soma)
+
